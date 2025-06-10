b17295dd0383a5109f615d887a3e1988d3811077
Apurv Chitransh <apurvchitransh.in@gmail.com>
2025-06-11 01:13:45 +0530

    Major Changes Wsignet

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import SuperUser from './pages/SuperUser'
 import './index.css'
-import App from './pages/App/App'
-import Auth from './pages/App/Auth'
 
-const router = createBrowserRouter([
-  {
-    path: "/superuser",
-    Component: SuperUser
-  },
-  {
-    path: "/:id",
-    Component: App,
-    children: [
-      {index: true, Component: Auth}
-    ]
-  }
-])
-
-createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>
-)
+createRoot(document.getElementById('root')).render()
--- client/src/pages/SuperUser.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SuperUser = () => {
-  return (
-    <div>SuperUser</div>
-  )
-}
-
-export default SuperUser
