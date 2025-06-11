2797692844d409a4eca5cd01fa9d9b9ed1020a61
Apurv Chitransh <apurvchitransh.in@gmail.com>
2025-06-12 02:17:48 +0530

    Add Dark Mode in the Client Side

---
+++ client/src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ThemeProvider } from './context/themeProvider.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <App />
+  </ThemeProvider>
+)
